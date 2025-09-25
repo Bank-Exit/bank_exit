c2193d542f50980cb6a457c402139c53442fd6f8
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-09-25 17:58:36 +0200

    feat: temporarily force admin side to french locale

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,16 @@ module Admin
   class BaseController < ApplicationController
     include HttpAuthConcern
 
+    around_action :switch_locale
+
     layout 'admin'
+
+    private
+
+    # Temporarily force the locale to French on the admin side
+    # until full i18n integration is implemented.
+    def switch_locale(&action)
+      I18n.with_locale(:fr, &action)
+    end
   end
 end
