ed570b8628d51a83a7765440f6cd112c352572f5
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-10-09 15:04:02 +0200

    refactor: DRY all modal partials and extract them into a single one

--- app/helpers/modals_helper.rb
+++ app/helpers/modals_helper.rb
@@ -1,5 +1,6 @@
 module ModalsHelper
   def render_modal(**, &block)
-    render('modal_template', **, &block)
+    modal_body_html = capture(&block) if block_given?
+    render('application/modal', **, modal_body_html: modal_body_html)
   end
 end
