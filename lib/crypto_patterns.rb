ef103d7f6e7da874814ce8d36293ef55e6d57eda
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-10-12 09:15:05 +0200

    fix: update the monero address regex pattern to be valid

--- lib/crypto_patterns.rb
+++ lib/crypto_patterns.rb
@@ -1,7 +1,7 @@
 module CryptoPatterns
   BITCOIN_ADDRESS_REGEX   = /\A(bc1|1|3)[a-zA-HJ-NP-Z0-9]{25,39}\z/
   LIGHTNING_INVOICE_REGEX = /\Alnbc[0-9a-zA-Z]{10,}\z/
-  MONERO_ADDRESS_REGEX    = /^(4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}|4[0-9AB][1-9A-HJ-NP-Za-km-z]{105}|8[1-9A-HJ-NP-Za-km-z]{94})$/
+  MONERO_ADDRESS_REGEX    = /\A(4[1-9A-HJ-NP-Za-km-z]{94}|4[1-9A-HJ-NP-Za-km-z]{105}|8[1-9A-HJ-NP-Za-km-z]{94})\z/
   JUNE_KEY_REGEX          = /\A[A-Za-z0-9]{42,55}\z/
 
   def self.detect_type(value)
