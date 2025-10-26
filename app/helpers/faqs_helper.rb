8b1a4857a412974ae745c29ff48f864432e7f9b6
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-10-26 16:27:05 +0100

    refactor: display referenced articles title in FAQ links instead of link

--- app/helpers/faqs_helper.rb
+++ app/helpers/faqs_helper.rb
@@ -10,7 +10,16 @@ module FAQsHelper
       controller, action, id = mustache.split(Regexp.union(delimiters))
 
       link = url_for(controller: controller, action: action, id: id)
-      answer.gsub!(mustache, link_to(link, link))
+
+      label = link
+
+      # Find article title to be used as text for link
+      if controller.in?(%w[tutorials blogs]) && id
+        resource = controller.classify.constantize.find(id)
+        label = resource.title
+      end
+
+      answer.gsub!(mustache, link_to(label, link))
             .gsub!('{{', '')&.gsub!('}}', '')
     end
 
