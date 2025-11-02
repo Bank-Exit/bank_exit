4061c9222032c1408a203263cefca21d337b6fe0
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-11-02 16:14:26 +0100

    fix: remove legacy `sessions` table from `schema.rb`

--- db/schema.rb
+++ db/schema.rb
@@ -277,15 +277,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_06_165850) do
     t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_text_translations_on_keys", unique: true
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -311,5 +302,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_06_165850) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "directories", "merchants"
-  add_foreign_key "sessions", "users"
 end
