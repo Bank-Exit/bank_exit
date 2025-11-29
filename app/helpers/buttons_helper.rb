355da5bdb0c93de6ac8f630ba3f8cafd95f6ef29
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-11-29 23:19:49 +0100

    refactor: DRY `link_to` views actions using `ButtonsHelper` methods

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -13,6 +13,28 @@ module ButtonsHelper
     end
   end
 
+  def create_link_to(link, label: t('create'), klass: 'btn btn-sm btn-success', turbo_confirm: t('create_confirm'))
+    link_to link, class: klass, data: { turbo_method: :post, turbo_confirm: turbo_confirm } do
+      if block_given?
+        yield
+      else
+        concat label
+        concat lucide_icon('circle-plus', class: 'inline-flex ml-1 w-4')
+      end
+    end
+  end
+
+  def show_link_to(link, label: t('see'), klass: 'btn btn-sm btn-info', data: {}, blank: false)
+    link_to link, class: klass, data: data, target: ('_blank' if blank) do
+      if block_given?
+        yield
+      else
+        concat label
+        concat lucide_icon('eye', class: 'inline-flex ml-1 w-4')
+      end
+    end
+  end
+
   def edit_link_to(link, label: t('edit'), klass: 'btn btn-sm btn-warning', data: {})
     link_to link, class: klass, data: data do
       concat label
@@ -20,8 +42,15 @@ module ButtonsHelper
     end
   end
 
-  def destroy_link_to(link, label: t('destroy'), klass: 'btn btn-sm btn-error', data: { turbo_method: :delete, turbo_confirm: t('destroy_confirm') })
-    link_to link, class: klass, data: data do
+  def update_link_to(link, label: t('update'), klass: 'btn btn-sm btn-success', turbo_confirm: t('update_confirm'))
+    link_to link, class: klass, data: { turbo_method: :patch, turbo_confirm: turbo_confirm } do
+      concat label
+      concat lucide_icon('pencil', class: 'inline-flex ml-1 w-4')
+    end
+  end
+
+  def destroy_link_to(link, label: t('destroy'), klass: 'btn btn-sm btn-error', turbo_confirm: t('destroy_confirm'))
+    link_to link, class: klass, data: { turbo_method: :delete, turbo_confirm: turbo_confirm } do
       concat label
       concat lucide_icon('trash', class: 'inline-flex ml-1 w-4')
     end
